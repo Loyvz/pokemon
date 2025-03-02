f848100b122856ceb40548d98f6e0587015a74d4
loyvz <luisgrvl@gmail.com>
2025-03-02 18:00:58 +0100

    Primer commit - Configuración de Terraform

---
+++ main.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region = "us-east-1" # Cambia la región si necesitas otra
+}
+
+resource "aws_s3_bucket" "pokemon_platino_bucket" {
+  bucket = "pokemon-platino-bucket-gvelez" # Cambia el nombre si ya está en uso
+}
